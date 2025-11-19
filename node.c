b24f16f944722bf16ab748f3b5e70a57b51ca8f5
sssrisha <sssrisha.ad24@bmsce.ac.in>
2025-11-19 14:39:38 +0530

    node.c

--- node.c
+++ node.c
@@ -1,28 +1,46 @@
-#include<stdio.h>
-#include<stdlib>
- struct node{
-  int data;
-  struct node *next;
-  };
+#include <stdio.h>
+#include <stdlib.h>
 
-  int mian()
-  {
-    struct node *head=NULL;
-    struct node *n1,*n2,*n3;
+struct node {
+    int data;
+    struct node *next;
+};
 
-    n1=(struct node*)malloc(sizeof(struct node));
-    n2=(struct node*)malloc(sizeof(struct node));
-    n3=(struct node*)malloc(sizeof(struct node));
+int main() {
+    struct node *head = NULL;
+    struct node *n1, *n2, *n3;
 
+    
+    n1 = (struct node*)malloc(sizeof(struct node));
+    n2 = (struct node*)malloc(sizeof(struct node));
+    n3 = (struct node*)malloc(sizeof(struct node));
 
+ 
+    printf("Enter value for node 1: ");
+    scanf("%d", &n1->data);
+
+    printf("Enter value for node 2: ");
+    scanf("%d", &n2->data);
+
+    printf("Enter value for node 3: ");
+    scanf("%d", &n3->data);
+
+ 
     head = n1;
-    n1->data=1;
-    n1->next=n2;
-    n2->data=2;
-    n2->next=n3;
-    n3->data=3;
-    n3->next=NULL;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = NULL;
 
-    struct node* temp;
-    temp=head;
-    temp=temp->next;
+   
+    struct node* temp = head;
+    printf("\nLinked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+    return 0;
+}
+
+
